30d99e9f0505b0bf17337596c5bf4545f27f7c94
krel99 <karlos.slaj@gmail.com>
2025-01-17 14:33:39 +0100

    still problems with statefulness

--- src/text_response_stateful.rs
+++ src/text_response_stateful.rs
@@ -9,13 +9,27 @@ const OPENAI_ENV_NAME: &str = "OPENAI_API_KEY";
 type MyDialogue = Dialogue<State, InMemStorage<State>>;
 type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
 
-// Default - instance creator
-// Debug - allows me to log State to console
-#[derive(Clone, Default)] // why clone necessary, what is default, when is debug used
+// #[derive(Clone)]
+// pub struct MyChatRequest {
+//     messages: Vec<ChatMessage>,
+// }
+
+// impl MyChatRequest {
+//     pub fn new(messages: Vec<ChatMessage>) -> Self {
+//         Self { messages }
+//     }
+
+//     pub fn add_message(&mut self, message: ChatMessage) {
+//         self.messages.push(message);
+//     }
+// }
+
+#[derive(Clone, Default)]
 pub enum State {
     #[default]
     Start,
-    Conversation {
+    Conversation,
+    MyChatRequest {
         messages: ChatRequest,
     },
 }
@@ -33,7 +47,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         bot,
         Update::filter_message()
             .enter_dialogue::<Message, InMemStorage<State>, State>()
-            .branch(dptree::case![State::Start].endpoint(start)),
+            .branch(dptree::case![State::Start].endpoint(start))
+            .branch(dptree::case![State::Conversation].endpoint(conversation)),
     )
     .dependencies(dptree::deps![InMemStorage::<State>::new()])
     .enable_ctrlc_handler()
@@ -41,33 +56,65 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .dispatch()
     .await;
 
-    println!("DISPATCHER ended");
-
     Ok(())
 }
 
 async fn start(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
-    println!("fn START");
+    match msg.text() {
+        Some(text) => {
+            let mut chat_req =
+                ChatRequest::new(vec![
+                    ChatMessage::system("Answer on the level of A1 speaker, then make open-ended statement or ask question."),
+                    ChatMessage::user(text),
+                ]);
+
+            let client = Client::default();
+            let model = MODEL_OPENAI;
+            let env_name = OPENAI_ENV_NAME;
+            // Skip if does not have the environment name set
+            if !env_name.is_empty() && std::env::var(env_name).is_err() {
+                println!("===== Skipping model: {model} (env var not set: {env_name})");
+            }
+            let chat_res = client.exec_chat(model, chat_req.clone(), None).await?;
+            let res = chat_res.content_text_into_string().unwrap_or_default();
+            bot.send_message(msg.chat.id, res.clone()).await?;
+            chat_req.messages.push(ChatMessage::assistant(res));
+
+            dialogue
+                .update(State::MyChatRequest {
+                    messages: chat_req.clone(),
+                })
+                .await?;
+
+            dialogue.update(State::Conversation).await?;
+
+            for message in &chat_req.messages {
+                println!("{:?}", message);
+            }
+        }
+        None => {
+            bot.send_message(msg.chat.id, "Send me plain text.").await?;
+        }
+    }
+    Ok(())
+}
+
+async fn conversation(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
+    println!("fn CONVERSATION");
     match msg.text() {
         Some(text) => {
             let existing_dialogue = dialogue.get().await;
 
-            let mut chat_req = if let Ok(Some(State::Conversation { messages })) = existing_dialogue
-            {
-                let mut new_messages = messages.messages.clone();
-                new_messages.push(ChatMessage::user(text));
-                ChatRequest::new(new_messages)
-            } else {
-                ChatRequest::new(vec![
-                    ChatMessage::system("Answer on the level of A1 speaker, then make open-ended statement or ask question."),
-                    ChatMessage::user(text),
-                ])
-            };
-
-            // let mut chat_req = ChatRequest::new(vec![
-            //     ChatMessage::system("Answer on the level of A1 speaker, then make open-ended statement or ask question."),
-            //     ChatMessage::user(text),
-            // ]);
+            // let mut chat_req =  dialogue::storage::InMemStorage::
+            let mut chat_req =
+                if let Ok(Some(State::MyChatRequest { messages })) = existing_dialogue {
+                    let mut new_messages = messages.messages.clone();
+                    new_messages.push(ChatMessage::user(text));
+                    ChatRequest::new(new_messages)
+                } else {
+                    println!("Probably writing too fast");
+                    panic!("");
+                };
 
             // To print a Vec of ChatMessages, we iterate through and print each
 
@@ -84,10 +131,12 @@ async fn start(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
             chat_req.messages.push(ChatMessage::assistant(res));
 
             dialogue
-                .update(State::Conversation {
+                .update(State::MyChatRequest {
                     messages: chat_req.clone(),
                 })
                 .await?;
+
+            dialogue.update(State::Conversation).await?;
 
             for message in &chat_req.messages {
                 println!("{:?}", message);
